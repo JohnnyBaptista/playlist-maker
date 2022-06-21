e9ac5af6110e5618b4818d317ccccdf23ab4a8b1
jbaptista2 <jbaptista@gobots.com.br>
2022-06-21 17:19:07 -0300

    endpoint de registro

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
+const UserModel = require("./models/UserModel");
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    res.send({ok: 'Funfando'})
-})
+routes.get("/", (req, res) => {
+  res.send({ ok: "Funfando" });
+});
+
+routes.post("/register", async (req, res) => {
+  const { name, password, age, email } = req.body;
+  const newUser = new UserModel({ name, password, age, email });
+
+  try {
+    const response = await newUser.save();
+    res.send({ response, newUser });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 module.exports = routes;
+
